c6dad91b47693d4633235fad1cad81e44db80e8b
Cordier Eugene <eugene126549@gmail.com>
2023-01-11 12:36:13 +0100

    Coordonnées implémentées :

--- Model/Coordonnee.py
+++ Model/Coordonnee.py
@@ -18,4 +18,29 @@ def type_coordonnee(coord: tuple) -> bool:
     return type(coord) == tuple and len(coord) == 2 and type(coord[0]) == int and type(coord[1]) == int \
         and coord[0] >= 0 and coord[1] >= 0
 
+def construireCoordonnee(numL: int, numC: int) -> tuple :
+    ''' la fonction sert à créer une coordonnée sous la forme d'un tuple(L, C) en recevant la ligne et la colonne'''
+    if type(numL) != int or type(numC) != int:
+        raise TypeError("construireCoordonnee : Le numéro de ligne type_du_premier_paramètre ou le numéro de colonne type_du_second_paramètre ne sont pas des entiers")
+    tuple=(numL, numC)
+    if numL<0 or numC<0:
+        raise ValueError("construireCoordonnee : Le numéro de ligne", numL," ou de colonne ", numC," ne sont pas positifs")
+    return tuple
+
+def getLigneCoordonnee (coordonnee: tuple) -> int:
+    ''' la fonction sert a récupérer la ligne dans une coordonnee'''
+    if type(coordonnee) != tuple:
+        raise TypeError(" getLigneCoordonnee : Le paramètre n’est pas une coordonnée ")
+    return coordonnee[0]
+coo=(1,2)
+
+def getColonneCoordonnee(coordonnee: tuple) -> int:
+    ''' la fonction sert a récupérer la colonne dans une coordonnee'''
+    if type(coordonnee)!=tuple:
+        raise TypeError(" getColonneCoordonnee : Le paramètre n’est pas une coordonnée ")
+    return coordonnee[1]
+
+
+
+
 
--- const.py
+++ const.py
@@ -25,3 +25,4 @@ class _Const:
 
 
 sys.modules[__name__] = _Const()
+
